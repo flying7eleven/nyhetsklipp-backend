3affbecb33f6576e95d93bce19489092397ff07d
Tim Janke <tim+github@janke.biz>
2022-02-02 13:11:38 +0100

    :construction: Just spin up a rocket instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::{json::Json, Deserialize};
+
+#[derive(Deserialize)]
+struct Selectors {
+    title: String,
+    full_text_link: String,
+}
+
+#[derive(Deserialize)]
+struct ArticleSourceParameters<'r> {
+    source: &'r str,
+    selectors: Selectors,
+}
+
+#[get("/live/articles/for", data = "<article_source>")]
+fn index(article_source: Json<ArticleSourceParameters<'_>>) -> String {
+    article_source.0.source.to_string()
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build().mount("/", routes![index]).launch().await;
 }
